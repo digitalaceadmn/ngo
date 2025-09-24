6a5865062fef6dddde7a6d3b0d30a6c2860c1120
abhi <abhisekh@digitalace.in>
2025-09-24 12:35:05 +0530

    added a admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Configuration
+
+
+@admin.register(Configuration)
+class ConfigurationAdmin(admin.ModelAdmin):
+    list_display = ("key", "value")
+    search_fields = ("key", "value")
+    list_per_page = 20
