d406c3db3c25915c3d976d4944f002a913cfada0
abhi <abhisekh@digitalace.in>
2025-10-01 00:02:22 +0530

    added a studen post api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
-from .models import DoctorApplication, NGOApplication, SupportApplication
+from .models import DoctorApplication, NGOApplication, SupportApplication, Student
+
 
 class DoctorApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorApplication
-        fields = '__all__'
+        fields = "__all__"
+
 
 class NGOApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = NGOApplication
-        fields = '__all__'
+        fields = "__all__"
+
 
 class SupportApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = SupportApplication
-        fields = '__all__'
+        fields = "__all__"
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
